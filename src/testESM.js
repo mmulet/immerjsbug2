82a8c9fa9d56cf896136b2707c3942ab4688230e
Michael Mulet <michael_mulet@latefordinnerstudios.com>
2020-03-21 06:23:01 -0700

    changed the reference

--- src/testESM.js
+++ src/testESM.js
@@ -1,3 +1,3 @@
-import {produce} from "https://unpkg.com/immer@6.0.2/dist/immer.esm.js"
-
+// import {produce} from "https://unpkg.com/immer@6.0.2/dist/immer.esm.js"
+import {produce} from "https://unpkg.com/immer@6.0.2/dist/immer.umd.production.min.js"
 console.log("esm test pass")
